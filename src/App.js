dd4f5e4e0ca910f3b73cfc80a3ad1e2b9f31dd88
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-07 11:06:52 -0300

    parte inicial api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from './components/layout';
 
-const App = () => {
+function App() {
   return <main>
     <Layout>
       <div>
-        <img src="https://avatars.githubusercontent.com/u/1?v=4" alt="avatar of user" />
+        <img src="https://avatars.githubusercontent.com/u/65916297?v=4" alt="Picture of user" />
         <h1>Jessica Costa</h1>
         <h3>Username: </h3>
         <span>jessicacosta07</span>
@@ -13,20 +14,16 @@ const App = () => {
           <span>5</span>
         </div>
         <div>
-          <h4>Starreds</h4>
+          <h4>Starredss</h4>
           <span>5</span>
         </div>
         <div>
-          <h4>Followers</h4>
-          <span>5</span>
-        </div>
-        <div>
-          <h4>Followind</h4>
+          <h4>Following</h4>
           <span>5</span>
         </div>
       </div>
       <div>Repositories</div>
-      <div>Starredes</div>
+      <div>Starreds</div>
     </Layout>
   </main>;
 }
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,12 @@
-import React, { Children } from 'react';
+import React from 'react';
 
-const Layout = ({ children }) => {
-    return <section>
-        <header>header</header>
-        {children}
-    </section>;
-};
+function Layout({ children }) {
+    return (
+        <section>
+            <header>header</header>
+            {children}
+        </section>
+    )
+}
 
 export default Layout;
