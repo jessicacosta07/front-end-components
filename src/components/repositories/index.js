62680136cc288589953d424a3361f2cd2f8ae182
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-07 17:17:10 -0300

    parte do repository

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RepositoryItem from '../repository-item';
 import * as S from './styled';
 
 const Repositories = () => {
@@ -9,8 +10,14 @@ const Repositories = () => {
             <S.WrapperTab>Repositories</S.WrapperTab>
             <S.WrapperTab>Starred</S.WrapperTab>
         </S.WrapperTabList>
-        <S.WrapperTabPanel>Panel Repositories</S.WrapperTabPanel>
-        <S.WrapperTabPanel>Panel Starred</S.WrapperTabPanel>
+        <S.WrapperTabPanel>
+            <RepositoryItem name='repo 1' linkToRepo='https://api.github.com/repos/jessicacosta07/dinoGameDIO'
+                fullName='jessicacosta07/dinoGameDIO' />
+        </S.WrapperTabPanel>
+        <S.WrapperTabPanel>
+            <RepositoryItem name='repo 2' linkToRepo='https://api.github.com/repos/jessicacosta07/challenge-gama'
+                fullName='jessicacosta07/challenge-gama' />
+        </S.WrapperTabPanel>
     </S.WrapperTabs>
 }
 
