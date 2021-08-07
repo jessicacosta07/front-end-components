5dd22742b0624a2ef259ca1694754fbfae367b5a
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-07 18:06:48 -0300

    parte provider

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import useGithub from '../../hooks/github-hooks';
 import * as S from './styled';
 
 const Profile = () => {
+    const { githubState } = useGithub()
     return (
         <S.Wrapper>
             <S.WrapperImage src="https://avatars.githubusercontent.com/u/65916297?v=4" alt="Picture of user" />
             <S.WrapperInfoUsers>
                 <div>
-                    <h1>Jessica Costa</h1>
+                    <h1>{githubState.user.name}</h1>
                     <S.WrapperUsername>
                         <h3>Username: </h3>
                         <a href="https://github.com/jessicacosta07" target="_blank" rel="noreferrer">jessicacosta07</a>
