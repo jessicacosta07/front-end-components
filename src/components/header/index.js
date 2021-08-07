f332fb599b5253f563884364ab7d0456fffedaf7
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-07 19:49:13 -0300

    parte login

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styled';
+import useGithub from '../../hooks/github-hooks';
 
 const Header = () => {
+    const { getUser } = useGithub();
+    const { usernameForSearch, setUsernameForSearch } = useState()
+    const submitGetUser = () => {
+        if (!usernameForSearch) return;
+        return getUser(usernameForSearch);
+    }
     return (
         <header>
             <S.Wrapper>
-                <input type="text" placeholder = "Digite o username para pesquisa"/>
-                <button type = "submit">Buscar</button>
+                <input type="text" placeholder="Digite o username para pesquisa" onChange={submitGetUser} />
+                <button type="submit">
+                    <span>Buscar</span
+                    ></button>
             </S.Wrapper>
         </header>
     );
--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
+import api from '../services/api';
 
 export const GithubContext = createContext({
     loading: false,
@@ -26,12 +27,27 @@ const GithubProvider = ({ children }) => {
         starred: [],
     });
 
-    const getUser = () =>{
-
+    const getUser = (username) => {
+        api.get(`users/${username}`).then(({ data: { user } }) => {
+            setGithubState(prevState => ({
+                ...prevState, user: {
+                    login: user.login,
+                    name: user.name,
+                    html_url: user.html_url,
+                    blog: user.blog,
+                    company: user.company,
+                    followers: user.followers,
+                    following: user.following,
+                    public_gists: user.public_gists,
+                    public_repos: user.public_repos,
+                }
+            }))
+        })
     }
 
     const contextValue = {
         githubState,
+        getUser: useCallback((username) => getUser(username), []),
     }
 
     return (
