99df2751f8b3fb31d434c153cf76a7dbd2e8f5a6
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-07 20:19:07 -0300

    parte profile

--- src/providers/github-provider.js
+++ src/providers/github-provider.js
@@ -28,33 +28,45 @@ const GithubProvider = ({ children }) => {
     });
 
     const getUser = (username) => {
-        api.get(`users/${username}`).then(({ data: { user } }) => {
-            setGithubState(prevState => ({
-                ...prevState, user: {
-                    login: user.login,
-                    name: user.name,
-                    html_url: user.html_url,
-                    blog: user.blog,
-                    company: user.company,
-                    followers: user.followers,
-                    following: user.following,
-                    public_gists: user.public_gists,
-                    public_repos: user.public_repos,
-                }
-            }))
-        })
-    }
+        setGithubState((prevState) => ({
+            ...prevState,
+            loading: !prevState.loading,
+        }));
 
-    const contextValue = {
-        githubState,
-        getUser: useCallback((username) => getUser(username), []),
-    }
+        api
+            .get(`users/${username}`)
+            .then(({ data }) => {
+                setGithubState((prevState) => ({
+                    ...prevState,
+                    hasUser: true,
+                    user: {
+                        id: data.id,
+                        avatar: data.avatar_url,
+                        login: data.login,
+                        name: data.name,
+                        html_url: data.html_url,
+                        blog: data.blog,
+                        company: data.company,
+                        location: data.location,
+                        followers: data.followers,
+                        following: data.following,
+                        public_gists: data.public_gists,
+                        public_repos: data.public_repos,
+                    },
+                }));
+            })
+        };
 
-    return (
-        <GithubContext.Provider value={contextValue}>
-            {children}
-        </GithubContext.Provider>
-    );
-};
+        const contextValue = {
+            githubState,
+            getUser: useCallback((username) => getUser(username), []),
+        };
 
-export default GithubProvider;
+        return (
+            <GithubContext.Provider value={contextValue}>
+                {children}
+            </GithubContext.Provider>
+        );
+    };
+
+    export default GithubProvider;
